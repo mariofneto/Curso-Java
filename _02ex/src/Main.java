e23946263757a335727df52b2f70740be96172ed
Mário Neto <marioo.netoo3@gmail.com>
2023-08-04 21:13:34 -0300

    update
    dia 09

--- _02ex/src/Main.java
+++ _02ex/src/Main.java
@@ -73,7 +73,7 @@ public class Main {
             tela a altura média das pessoas, e mostrar também a porcentagem de pessoas com menos de 16 anos,
             bem como os nomes dessas pessoas caso houver.
          */
-
+        /*
         System.out.print("Quantas pessoas serão digitadas? ");
         int N = sc.nextInt();
 
@@ -88,7 +88,7 @@ public class Main {
         for(int c = 0; c < N; c++){
             System.out.println("Dados da "+ (c+1) +"a pessoa:");
             System.out.print("Nome: ");
-            nomes[c] = sc.nextLine();
+            nomes[c] = sc.next();
             sc.nextLine();
             System.out.print("Idade: ");
             idades[c] = sc.nextInt();
@@ -99,17 +99,132 @@ public class Main {
 
             if(idades[c] < 16){
                 qtdPessoasMenores16++;
+
             }
         }
 
-        // porcentagemmenor16 = qtdpessoasmenores16 * (1.0 / N)
-        double porcentagemMenor16 = qtdPessoasMenores16 * (1.0 / N);
+        // porcentagemmenor16 = (qtdpessoasmenores16 * (1.0 / N)) * 100
+        double porcentagemMenor16 = (qtdPessoasMenores16 * (1.0 / N)) * 100;
 
         alturaMedia = alturaSoma / N;
 
         System.out.printf("%nAltura média: %.2f%n", alturaMedia);
-        System.out.println(qtdPessoasMenores16);
-        System.out.printf("Pessoas com menos de 16 anos: %.2f%n", porcentagemMenor16);
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", porcentagemMenor16);
+
+        for(int c = 0; c < N; c++){
+            if(idades[c] < 16){
+                System.out.println(nomes[c]);
+            }
+        }
+        */
+
+        /*
+            Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na
+            tela todos os números pares, e também a quantidade de números pares
+         */
+
+        /*System.out.print("Quantos numeros voce vai digitar? ");
+        int resposta = sc.nextInt();
+        int [] numeros = new int[resposta];
+        int quantidadePares = 0;
+
+        for(int c = 0; c < numeros.length; c++){
+            System.out.print("Digite um numero: ");
+            numeros[c] = sc.nextInt();
+        }
+
+        System.out.println("NUMEROS PARES: ");
+
+        for (int i = 0; i < numeros.length; i++){
+            if(numeros[i] % 2 == 0){
+                System.out.print(numeros[i] + " ");
+                quantidadePares++;
+            }
+        }
+        System.out.println(" ");
+        System.out.print("QUANTIDADE DE PARES = " + quantidadePares);
+        */
+
+        /*
+        Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela
+        o maior número do vetor (supor não haver empates). Mostrar também a posição do maior elemento,
+        considerando a primeira posição como 0 (zero).
+         */
+        /*
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int resposta = sc.nextInt();
+        double[] numerosReais = new double[resposta];
+        double maior = 0;
+        int posicao = 0;
+
+        for(int c = 0; c < numerosReais.length; c++){
+            System.out.print("Digite um numero: ");
+            numerosReais[c] = sc.nextDouble();
+
+            if(numerosReais[c] > maior){
+                maior = numerosReais[c];
+                posicao = c;
+            }
+        }
+
+        System.out.println("MAIOR VALOR = "+ maior);
+        System.out.println("POSICAO DO MAIOR VALOR = "+ posicao);
+        */
+
+        /*
+        Faça um programa para ler dois vetores A e B, contendo N elementos cada. Em seguida, gere um
+        terceiro vetor C onde cada elemento de C é a soma dos elementos correspondentes de A e B. Imprima
+        o vetor C gerado.
+         */
+
+        /*System.out.print("Quantos valores vai ter cada vetor? ");
+        int resposta = sc.nextInt();
+
+        int [] vetorA = new int[resposta];
+        int [] vetorB = new int[resposta];
+        int [] vetorC = new int[resposta];
+
+        System.out.println("Digite os valores do vetor A:");
+        for(int c = 0; c < vetorA.length; c++){
+            vetorA[c] = sc.nextInt();
+
+        }
+
+        System.out.println("Digite os valores do vetor B:");
+        for(int c = 0; c < vetorB.length; c++){
+            vetorB[c] = sc.nextInt();
+
+        }
+
+        System.out.println("VETOR RESULTANTE: ");
+        for(int c = 0; c < vetorC.length; c++){
+            System.out.println(vetorA[c] + vetorB[c]);
+
+        }
+        */
+
+        /*
+        Fazer um programa para ler um número inteiro N e depois um vetor de N números reais. Em seguida,
+        mostrar na tela a média aritmética de todos elementos com três casas decimais. Depois mostrar todos
+        os elementos do vetor que estejam abaixo da média, com uma casa decimal cada.
+         */
+
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int resposta = sc.nextInt();
+
+        double[] vetor = new double[resposta];
+        double soma;
+        double media;
+
+        for(int c = 0; c < vetor.length; c++){
+            System.out.print("Digite um numero: ");
+            vetor[c] = sc.nextDouble();
+            soma += vetor[c];
+        }
+
+
+
+
 
 
 
